fe4a7205a45f3f2822b6a54f1b90e0be683b7af9
Matthew_Yu <matthewjkyu@.gmail.com>
2018-07-02 18:12:52 -0500

    update to ui, added starfield entrace, adjustments to section margins for better full screen experience, added close button to main sections, preliminary color scheme redesign

---
+++ Starfield.js
@@ -0,0 +1,23 @@
+/**
+ * Main sketch for starfield canvas using p5.js
+ * Matthew Yu
+ */
+var stars = [];
+var speed = 10; //modify speed of star movement
+
+function setup() {
+  var cnv = createCanvas(window.innerWidth * .988, window.innerHeight);
+  cnv.parent('starfield');
+  for(var i = 0; i < 500; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
